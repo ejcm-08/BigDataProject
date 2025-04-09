bf34d3ffb23a70ea1aa168d023319a67499bfed7
Ed Jerome Manalo <manaloedjerome@gmail.com>
2025-04-09 23:03:44 +0800

    Added hero-section and some cards. Initial design

--- btnpop-app/src/App.js
+++ btnpop-app/src/App.js
@@ -1,18 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-import react from 'react';
-import { useState } from 'react';
+import React from 'react';
 import Navbar from './Components/Navbar/navbar';
 import Footer from './Components/Footer/footer';
+import heroImage from './Content/Images/rari.jpg';
+import './App.css';
 
 function App() {
   return (
     <div className="container">
-     <Navbar />
+      <Navbar />
 
-     <div className="footer">
-     <Footer />
-     </div>
+      <div className="hero-section">
+        <div className="hero-content">
+          <h1>MUSIC ✕ VIDEO FEST</h1>
+          <p>
+            BTN POP is a music and video event which brings together talents in the province of Bataan in a multi-media affair.
+          </p>
+          <p>
+            SONGS submitted by local songwriters interpreted by upcoming home-grown artists, featured in a MUSIC VIDEO directed by local filmmakers, will all lead up to an exciting LIVE EVENT!
+          </p>
+          <button className="hero-button">WATCH INTRO</button>
+        </div>
+        <div className="hero-image">
+          <img src={heroImage} alt="Hero" />
+        </div>
+      </div>
+
+      {/* For Users to Click On */}
+      <div className="cards-container">
+        <div className="card">
+          <h3>Songwriters</h3>
+          <p>Be part of the first batch of songwriters for BTN POP</p>
+        </div>
+        <div className="card">
+          <h3>Artists</h3>
+          <p>Be part of the BTN POP Album comprised of original songs chosen from the BTN POP Songwriting Competition</p>
+        </div>
+        <div className="card">
+          <h3>Directors</h3>
+          <p>Be part of history as you visualize the songs taken from the forthcoming BTN POP Album</p>
+        </div>
+      </div>
+
+      <Footer />
     </div>
   );
 }

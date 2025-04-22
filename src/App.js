85dfd63a6852c093d51f68c5d54532656c2b5e77
Ed Jerome Manalo <manaloedjerome@gmail.com>
2025-04-22 18:21:41 +0800

    New shht

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ import './App.css';
 function App() {
   return (
     <div className="container">
-      <Navbar />
-
+            <Navbar />
       <div className="hero-section">
         <div className="hero-content">
           <h1>MUSIC ✕ VIDEO FEST</h1>
@@ -18,26 +17,9 @@ function App() {
           <p>
             SONGS submitted by local songwriters interpreted by upcoming home-grown artists, featured in a MUSIC VIDEO directed by local filmmakers, will all lead up to an exciting LIVE EVENT!
           </p>
-          <button className="hero-button">WATCH INTRO</button>
         </div>
         <div className="hero-image">
           <img src={heroImage} alt="Hero" />
-        </div>
-      </div>
-
-      {/* For Users to Click On */}
-      <div className="cards-container">
-        <div className="card">
-          <h3>Songwriters</h3>
-          <p>Be part of the first batch of songwriters for BTN POP</p>
-        </div>
-        <div className="card">
-          <h3>Artists</h3>
-          <p>Be part of the BTN POP Album comprised of original songs chosen from the BTN POP Songwriting Competition</p>
-        </div>
-        <div className="card">
-          <h3>Directors</h3>
-          <p>Be part of history as you visualize the songs taken from the forthcoming BTN POP Album</p>
         </div>
       </div>
 
--- src/Components/Navbar/navbar.js
+++ src/Components/Navbar/navbar.js
@@ -1,14 +1,18 @@
 import './navbar.css';
 import React from 'react';
-import { useState } from 'react';
 
 function Navbar() {
     return (
         <div className="navbar">
-            <img src="https://via.placeholder.com/150" alt="" className="navbar-logo" />
-            <div className="navbar-company">BTNPop</div>
+            <img src="https://via.placeholder.com/40" alt="Logo" className="navbar-logo" />
+            <div className="navbar-company">BTNASSOCIATION</div>
             <div className="navbar-menu">
-
+                <ul className="navbar-list">
+                    <li className="navbar-item"><a href="#home">Home</a></li>
+                    <li className="navbar-item"><a href="#about">News</a></li>
+                    <li className="navbar-item"><a href="#services">Events</a></li>
+                    <li className="navbar-item"><a href="#contact">Donate</a></li>
+                </ul>
             </div>
         </div>
     );

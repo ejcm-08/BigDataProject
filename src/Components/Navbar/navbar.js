d9ddff560fe70f4068005dd1611842f52397b44c
Ed Jerome Manalo <manaloedjerome@gmail.com>
2025-04-29 17:43:08 +0800

    Added Router and pages

--- src/Components/Navbar/navbar.js
+++ src/Components/Navbar/navbar.js
@@ -1,17 +1,17 @@
-import './navbar.css';
+import './navbar.css';  
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
         <div className="navbar">
-            <img src="https://via.placeholder.com/40" alt="Logo" className="navbar-logo" />
             <div className="navbar-company">BTNASSOCIATION</div>
             <div className="navbar-menu">
                 <ul className="navbar-list">
-                    <li className="navbar-item"><a href="#home">Home</a></li>
-                    <li className="navbar-item"><a href="#about">News</a></li>
-                    <li className="navbar-item"><a href="#services">Events</a></li>
-                    <li className="navbar-item"><a href="#contact">Donate</a></li>
+                    <li className="navbar-item"><Link to="/">Home</Link></li>
+                    <li className="navbar-item"><Link to="/news">News</Link></li>
+                    <li className="navbar-item"><Link to="/events">Events</Link></li>
+                    <li className="navbar-item"><Link to="/donate">Donate</Link></li>
                 </ul>
             </div>
         </div>
---
+++ src/pages/NewsPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NewsPage() {
+  return (
+    <div className="container">
+      <h1>News</h1>
+    </div>
+  );
+}
+
+export default NewsPage;

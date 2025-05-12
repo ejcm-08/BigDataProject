9dcd9bec236631f4ac775d037d212bdcc2bb253a
Ed Jerome Manalo <manaloedjerome@gmail.com>
2025-05-12 21:13:30 +0800

    About Page and chuchu

--- src/Components/Navbar/navbar.js
+++ src/Components/Navbar/navbar.js
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
-        <div className="navbar">
+        <div className="navbar section-content">
             <div className="navbar-company">BTNASSOCIATION</div>
             <div className="navbar-menu">
                 <ul className="navbar-list">
-                    <li className="navbar-item"><Link to="/">Home</Link></li>
-                    <li className="navbar-item"><Link to="/news">News</Link></li>
-                    <li className="navbar-item"><Link to="/events">Events</Link></li>
-                    <li className="navbar-item"><Link to="/donate">Donate</Link></li>
+                    <li className="navbar-item"><Link to="/">HOME</Link></li>
+                    <li className="navbar-item"><Link to="/news">NEWS</Link></li>
+                    <li className="navbar-item"><Link to="/events">EVENTS</Link></li>
+                    <li className="navbar-item"><Link to="/donate">ABOUT</Link></li>
                 </ul>
             </div>
         </div>
--- src/pages/NewsPage.js
+++ src/pages/NewsPage.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import './newspage.css';
 
 function NewsPage() {
   return (
-    <div className="container">
-      <h1>News</h1>
+    <div className="news-container">
+      <section className="weekly-highlight">
+        <h2 className="section-title">Weekly Highlight</h2>
+        <div className="highlight-card">
+          <div className="highlight-image">
+            <img src="https://via.placeholder.com/600x400" alt="Weekly Highlight" />
+          </div>
+          <div className="highlight-content">
+            <h3>BTN Association, as represented by Sir Gabriel Jessie Guevarra, participated in the Balanga City's Creative Burst Workshop.</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ornare ipsum sit amet urna feugiat, venenatis venenatis neque viverra.</p>
+            <p>Fusce nulla risus, tristique nec mollis consequat, blandit sed est. Sed vehicula nibh eu nunc tristique, quis feugiat quam hendrerit.</p>
+            <span className="date">Date: January 1 2025</span>
+          </div>
+        </div>
+      </section>
+
+      <section className="latest-news">
+        <h2 className="section-title">Latest News</h2>
+        <div className="news-grid">
+          <div className="news-card">
+            <img src="https://via.placeholder.com/300x200" alt="News 1" />
+            <h4>BTN Association, as represented by Sir Gabriel Jessie Guevarra, participated in the Balanga City's Creative Burst Workshop.</h4>
+            <span className="date">Date: January 1 2025</span>
+          </div>
+          <div className="news-card">
+            <img src="https://via.placeholder.com/300x200" alt="News 2" />
+            <h4>BTN Association, as represented by Sir Gabriel Jessie Guevarra, participated in the Balanga City's Creative Burst Workshop.</h4>
+            <span className="date">Date: January 1 2025</span>
+          </div>
+          <div className="news-card">
+            <img src="https://via.placeholder.com/300x200" alt="News 3" />
+            <h4>BTN Association, as represented by Sir Gabriel Jessie Guevarra, participated in the Balanga City's Creative Burst Workshop.</h4>
+            <span className="date">Date: January 1 2025</span>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }

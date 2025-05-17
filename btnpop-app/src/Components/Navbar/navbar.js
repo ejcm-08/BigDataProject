eeb5035f951c93c8bd3fb28f2297b2a24d53f337
Ed Jerome Manalo <manaloedjerome@gmail.com>
2025-05-17 19:12:42 +0800

    UMAYOS KA

--- btnpop-app/src/Components/Navbar/navbar.js
+++ btnpop-app/src/Components/Navbar/navbar.js
@@ -5,9 +5,10 @@ import { Link } from 'react-router-dom';
 function Navbar() {
     return (
         <div className="navbar">
-            <div className="navbar-company">BTNASSOCIATION</div>
+            <div className="navbar-company"><Link to="/">BTNASSOCIATION</Link></div>
             <div className="navbar-menu">
                 <ul className="navbar-list">
+
                     <li className="navbar-item"><Link to="/">HOME</Link></li>
                     <li className="navbar-item"><Link to="/news">NEWS</Link></li>
                     <li className="navbar-item"><Link to="/events">EVENTS</Link></li>
